9fdfa9b1ec0b980a00847a2b73db095dc0463ecd
Phuong Duong <phuongx31@gmail.com>
2020-01-04 14:08:15 +0700

    add day01 source

---
+++ Day01/main.py
@@ -0,0 +1,9 @@
+print("Welcome to the Brand Name Generator.")
+
+city = input("What is the name of the city you grew up?\n")
+
+pet = input("What is your pet name?\n")
+
+brand_name = city + " " + pet
+
+print("\nYour Brand Name could be: " + brand_name)
